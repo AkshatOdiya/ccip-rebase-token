dc6c9a7e7b5688d6143bec1bfb14ee4017cf65ec
AkshatOdiya <akshatodiya79@gmail.com>
2025-07-20 10:27:06 +0530

    Modified

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -3,7 +3,8 @@
 pragma solidity ^0.8.20;
 
 interface IRebaseToken {
-    function mint(address _to, uint256 _amount) external;
+    function mint(address _to, uint256 _amount, uint256 _userInterestRate) external;
     function burn(address _from, uint256 _amount) external;
     function balanceOf(address _user) external view returns (uint256);
+    function getUserInterestRate(address _user) external view returns (uint256);
 }
