27d5aa55c6fc8649e7a696b4da0800f87c5391d3
AkshatOdiya <akshatodiya79@gmail.com>
2025-07-21 12:10:58 +0530

    modified

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -26,12 +26,13 @@ import {Test, console} from "forge-std/Test.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {Vault} from "../src/Vault.sol";
-
+import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 import {RegistryModuleOwnerCustom} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
 import {TokenAdminRegistry} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
+import {RateLimiter} from "@ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
 
 contract CrossChainTest is Test {
     uint256 sepoliaFork;
@@ -75,6 +76,14 @@ contract CrossChainTest is Test {
         TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress).setPool(
             address(sepoliaToken), address(sepoliaTokenPool)
         );
+        configureTokenPool(
+            sepoliaFork, // Local chain: Sepolia
+            address(sepoliaTokenPool), // Local pool: Sepolia's TokenPool
+            arbSepoliaNetworkDetails.chainSelector, // Remote chain selector: Arbitrum Sepolia's
+            address(arbSepoliaTokenPool), // Remote pool address: Arbitrum Sepolia's TokenPool
+            address(arbSepoliaToken) // Remote token address: Arbitrum Sepolia's Token
+        );
+
         vm.stopPrank();
 
         // 2. Deploy and consifure on arbitrum sepolia
@@ -96,6 +105,36 @@ contract CrossChainTest is Test {
         TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress).setPool(
             address(arbSepoliaToken), address(arbSepoliaTokenPool)
         );
+        configureTokenPool(
+            arbSepoliaFork, // Local chain: Arbitrum Sepolia
+            address(arbSepoliaTokenPool), // Local pool: Arbitrum Sepolia's TokenPool
+            sepoliaNetworkDetails.chainSelector, // Remote chain selector: Sepolia's
+            address(sepoliaTokenPool), // Remote pool address: Sepolia's TokenPool
+            address(sepoliaToken) // Remote token address: Sepolia's Token
+        );
         vm.stopPrank();
+    }
+
+    function configureTokenPool(
+        uint256 fork,
+        address localPool,
+        uint64 remoteChainSelector,
+        address remotePool,
+        address remoteTokenAddress
+    ) public {
+        vm.selectFork(fork);
+        vm.prank(i_owner);
+        bytes[] memory remotePoolAddresses = new bytes[](1);
+        remotePoolAddresses[0] = abi.encode(remotePool);
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            allowed: true,
+            remotePoolAddress: abi.encode(remotePoolAddresses),
+            remoteTokenAddress: abi.encode(remoteTokenAddress),
+            outboundRateLimiterConfig: RateLimiter.Config({isEnabled: false, capacity: 0, rate: 0}),
+            inboundRateLimiterConfig: RateLimiter.Config({isEnabled: false, capacity: 0, rate: 0})
+        });
+        TokenPool(localPool).applyChainUpdates(chainsToAdd);
     }
 }
