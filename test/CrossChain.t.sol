7b1c8a598fab21b0bc8045cb9f49b6d7519bd867
AkshatOdiya <akshatodiya79@gmail.com>
2025-07-21 22:04:46 +0530

    Modified

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -1,25 +1,4 @@
 // SPDX-License-Identifier: MIT
-
-// Layout of Contract:
-// version
-// imports
-// interfaces, libraries, contracts
-// errors
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
 pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
